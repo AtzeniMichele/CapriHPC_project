1634bb260141fe7420e53ed5b6387b90882d8f6a
Michele <michele.atzeni@studenti.unipd.it>
2022-02-28 14:43:55 +0100

    # Update 8: improving the paper pipeline to see if results would change

--- NNscript2.py
+++ NNscript2.py
@@ -1,4 +1,5 @@
 # 1. Importing libraries
+import os
 import signal
 from timeit import default_timer as timer
 import pandas as pd
@@ -6,6 +7,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 import multiprocessing
+import os
 
 
 # 2. functions for the NN parallel training using multiprocessing library
@@ -20,14 +22,26 @@ def train_model(seed):
 
     # training e test set partitions
     train_features, test_features, train_outcome, test_outcome = train_test_split(features, outcome,
-                                                                                  test_size=0.3,
+                                                                                  test_size=0.2,
                                                                                   random_state=seed)
+
+    # data scaling
+    scaler = StandardScaler()
+    scaler.fit(train_features)
+
+    target_scaler = StandardScaler()
+    target_scaler.fit(train_outcome)
+
+    train_features = scaler.transform(train_features)
+    test_features = scaler.transform(test_features)
+    train_outcome = target_scaler.transform(train_outcome)
+    test_outcome = target_scaler.transform(test_outcome)
 
     learning_rate = 0.001
     epochs = 100
     batch_size = 10
     model = keras.Sequential([
-        keras.layers.Dense(units=50, input_dim=10, activation='relu'),
+        keras.layers.Dense(units=50, input_dim=18, activation='relu'),
         keras.layers.Dense(units=50, activation='relu'),
         keras.layers.Dense(1, kernel_initializer='normal')
     ])
@@ -55,45 +69,37 @@ def train_model(seed):
     # MAE = (abs(predict_test - test_outcome).sum()) / len(predict_test)
     # MSE = (((predict_test - test_outcome) ** 2).sum()) / len(predict_test)
 
-    MAE = metrics.mean_absolute_error(test_outcome,predict_test)
-    MSE = metrics.mean_squared_error(test_outcome,predict_test)
-    R_2 = metrics.r2_score(test_outcome,predict_test)
+    MAE = metrics.mean_absolute_error(test_outcome, predict_test)
+    MSE = metrics.mean_squared_error(test_outcome, predict_test)
+    R_2 = metrics.r2_score(test_outcome, predict_test)
 
     return {'seed': seed, 'test R^2': R_2, 'test MAE': MAE, 'test MSE': MSE}
 
 
 # 3. Loading, pre-processing and data partition
-#ds = pd.read_csv('/capri_project/210526WeAdatasetRegression.csv')
+# ds = pd.read_csv('/capri_project/210526WeAdatasetRegression.csv')
 ds = pd.read_csv('210526WeAdatasetRegression.csv')
 
-ds.drop(ds.columns[[0, 1]], axis=1,
+ds.drop(['UserNo.', 'UserID', 'SmokingHabit'], axis=1,
         inplace=True)  # first two columns are subject identifier, i.e are not useful to predict asthma exacerbations
-filtered_columns = ds.dtypes[ds.dtypes == np.object]
-list_columns = list(filtered_columns.index)
-for column in list_columns:
-    ds[column] = LabelEncoder().fit_transform(ds[column])
+ds = pd.get_dummies(ds, drop_first=True)  # categorical variables are dummified
+
+# filtered_columns = ds.dtypes[ds.dtypes == np.object]
+# list_columns = list(filtered_columns.index)
+# for column in list_columns:
+#     ds[column] = LabelEncoder().fit_transform(ds[column])
 
 outcome = np.array(ds['ACTScore'])
 outcome = outcome.reshape(-1, 1)
 
 print('outcome: ACTScore')
-features = ds.drop(['ACTScore','SmokingHabit'], axis=1)  # predictors of the model
+features = ds.drop('ACTScore', axis=1)  # predictors of the model
 print('features: ', list(features.columns))
 features = np.array(features)
 
-# data scaling
-scaler = StandardScaler()
-scaler.fit(features)
-
-target_scaler = StandardScaler()
-target_scaler.fit(outcome)
-
-features = scaler.transform(features)
-outcome = target_scaler.transform(outcome)
-
 # 4. Run the model
 start = timer()
-num_workers = 10
+num_workers = os.cpu_count()
 seeds = range(10)
 multiprocessing.set_start_method('fork', force=True)
 pool = multiprocessing.Pool(num_workers, init_worker)
@@ -103,12 +109,13 @@ end = timer()
 print(scores)
 
 cv_r2 = [list(scores[i].values())[1] for i in range(len(scores))]
-print('R^2 mean: {}  and standard deviation: {}'.format(np.mean(cv_r2),np.std(cv_r2)))
+print('R^2 mean: {}  and standard deviation: {}'.format(np.mean(cv_r2), np.std(cv_r2)))
 
 cv_mae = [list(scores[i].values())[2] for i in range(len(scores))]
-print('MAE mean: {}  and standard deviation: {}'.format(np.mean(cv_mae),np.std(cv_mae)))
+print('MAE mean: {}  and standard deviation: {}'.format(np.mean(cv_mae), np.std(cv_mae)))
 
 cv_mse = [list(scores[i].values())[3] for i in range(len(scores))]
-print('MSE mean: {}  and standard deviation: {}'.format(np.mean(cv_mse),np.std(cv_mse)))
+print('MSE mean: {}  and standard deviation: {}'.format(np.mean(cv_mse), np.std(cv_mse)))
 
 print('Elapsed time:', end - start, '[s]')
+print('Number of workers (e.g. CPUs):', num_workers)
