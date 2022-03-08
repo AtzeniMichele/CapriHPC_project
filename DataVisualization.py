b1aa7b1683daf482624b5bbe3f19e260d32463a6
Michele Atzeni <michele.atzeni@studenti.unipd.it>
2022-03-08 11:15:51 +0100

    update 10: new visualizations

--- DataVisualization.py
+++ DataVisualization.py
@@ -1,33 +1,34 @@
 import matplotlib.pyplot as plt
 
 # elapsed time using 1,2,4,8 cpus for parallel computing
-plt.plot([1,2,4,8],[111, 101, 66, 34],'ro')
-plt.axis([0, 9, 0, 120])
+plt.plot([1,2,4],[1429, 830, 423],'ro--')
+plt.axis([0, 9, 0, 1500])
 plt.xlabel('Number of CPUs')
 plt.ylabel('Elapsed time [s]')
 plt.show()
 
 #MSE mean value
-plt.errorbar([1,2,4,8],[0.12883458297796552, 0.12715138152343763, 0.13355227139543216, 0.1319459217099502],[0.017791448420769004,0.021731807905067593,0.02383135967412193,0.028117900490724778],linestyle='None',marker ='^',color='r',capsize = 3)
+plt.errorbar([1,2,4],[0.14788220337650443,0.13833703978414208,0.15133289396671432],[0.04053566136473489,0.018074495478963913,0.03061780346487201],linestyle='None',marker ='^',color='r',capsize = 3)
 #plt.axis([0, 9, 0.120, 0.140])
 plt.xlabel('Number of CPUs')
 plt.ylabel('MSE mean (sd)')
 plt.show()
 
 #MAE mean value
-plt.errorbar([1,2,4,8],[0.2412873383947008, 0.23610423270881378, 0.2410540344109542, 0.2404229220016229],[0.017851388969616567,0.020808020951085585,0.0210351619850076,0.021785963808881502],linestyle='None',marker ='^',color='b',capsize = 3)
+plt.errorbar([1,2,4],[0.22741584782668856,0.2277953658889897,0.23273514556763036],[0.022851533457693644,0.015424730243707302,0.018499025255352467],linestyle='None',marker ='^',color='b',capsize = 3)
 plt.xlabel('Number of CPUs')
 plt.ylabel('MAE mean (sd)')
 plt.show()
 
 #wallclock time
-plt.plot([1,2,4,8], [115, 102, 54, 36],'ro-')
+plt.plot([1,2,4], [1440, 840, 420],'ro--')
+plt.axis([0, 9, 0, 1500])
 plt.xlabel('Number of CPUs')
 plt.ylabel('Wall-Clock time [s]')
 plt.show()
-
+#
 #RAM Usage
-plt.plot([1,2,4,8], [0.56, 0.93, 1.62, 3.15],'bo-')
+plt.plot([1,2,4], [0.86,1.24,1.98],'bo-')
 plt.xlabel('Number of CPUs')
 plt.ylabel('RAM Usage [GB]')
 plt.show()
